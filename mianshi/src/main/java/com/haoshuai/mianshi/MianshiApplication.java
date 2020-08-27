b9f5dad1e3987fbbe82b854edcf225a89cfe8d7d
HaoShuai <haoshuai_nuc@163.com>
2020-08-27 15:25:41 +0800

    提交

---
+++ mianshi/src/main/java/com/haoshuai/mianshi/MianshiApplication.java
@@ -0,0 +1,13 @@
+package com.haoshuai.mianshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MianshiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MianshiApplication.class, args);
+    }
+
+}
