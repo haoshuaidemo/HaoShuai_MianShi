5e42e636b6b997664f53f3fd713b1f2d3c8d4150
HaoShuai <haoshuai_nuc@163.com>
2020-09-27 15:42:57 +0800

    上传

--- mianshi/src/main/java/com/haoshuai/mianshi/PractiseApplication.java
+++ mianshi/src/main/java/com/haoshuai/mianshi/PractiseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MianshiApplication {
+public class PractiseApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MianshiApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(PractiseApplication.class, args);
+	}
 
 }
---
+++ mianshi/src/main/java/com/haoshuai/mianshi/thread/Demo01.java
@@ -0,0 +1,11 @@
+package com.haoshuai.mianshi.thread;
+
+/**
+ * @author Hao Shuai
+ * @Title: Demo01
+ * @ProjectName mianshi
+ * @Description: TODO
+ * @date 2020/9/2715:42
+ */
+public class Demo01 {
+}
